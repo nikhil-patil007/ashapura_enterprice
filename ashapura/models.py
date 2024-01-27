0cfc6f62f33d9b6c7f94280f703948ff4ef24c44
nikhil-patil007 <nikhilpatil3701@gmail.com>
2024-01-27 22:06:27 +0530

    dev: Changed the models file to table.

--- ashapura/models.py
+++ ashapura/models.py
@@ -9,20 +9,20 @@ class User(models.Model):
     password = models.CharField( max_length=255,null=True,blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
-# class Vehicledetails(models.Model):
-#     customer_name = models.CharField( max_length=255,null=True,blank=True)
-#     account_no = models.CharField( max_length=255,null=True,blank=True)
-#     center = models.CharField( max_length=255,null=True,blank=True)
-#     executive = models.CharField( max_length=255,null=True,blank=True)
-#     segment = models.CharField( max_length=255,null=True,blank=True)
-#     product_name = models.CharField( max_length=255,null=True,blank=True)
-#     new_vehicle_number = models.CharField( max_length=255,null=True,blank=True)
-#     engine_number = models.CharField( max_length=255,null=True,blank=True)
-#     chasis_number = models.CharField( max_length=255,null=True,blank=True)
-#     created_at = models.DateTimeField(auto_now_add=True)
+class Vehicledetails(models.Model):
+    customer_name = models.CharField( max_length=255,null=True,blank=True)
+    account_no = models.CharField( max_length=255,null=True,blank=True)
+    center = models.CharField( max_length=255,null=True,blank=True)
+    executive = models.CharField( max_length=255,null=True,blank=True)
+    segment = models.CharField( max_length=255,null=True,blank=True)
+    product_name = models.CharField( max_length=255,null=True,blank=True)
+    new_vehicle_number = models.CharField( max_length=255,null=True,blank=True)
+    engine_number = models.CharField( max_length=255,null=True,blank=True)
+    chasis_number = models.CharField( max_length=255,null=True,blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     
-#     class Meta:
-#         db_table = "AshaPura_Vehicle_details"
+    class Meta:
+        db_table = "AshaPura_Vehicle_details"
     
-#     def __str__(self):
-#         return self.customer_name
+    def __str__(self):
+        return self.customer_name
