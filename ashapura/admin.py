5669f1cfc4a3b1d6c323eb8b861a77fd1af12074
nikhil-patil007 <nikhilpatil3701@gmail.com>
2024-01-27 22:04:45 +0530

    dev: Chnaged the models.

--- ashapura/admin.py
+++ ashapura/admin.py
@@ -7,15 +7,15 @@ admin.site.site_header = 'Ashapura Administration'
 admin.site.site_title = 'Ashapura Admin'
 admin.site.index_title = 'Welcome to Ashapura Enterprise Admin'
 
-class vehicalDetailsAdmin(admin.ModelAdmin):
-    # model = Categories
-    list_per_page = 15 # No of records per page 
-    list_display = ('id',"customer_name","account_no","center","executive","segment","product_name","new_vehicle_number","engine_number","chasis_number","created_at")
-    list_display_links = ('id',"customer_name","account_no","center","executive","segment","product_name","new_vehicle_number","engine_number","chasis_number","created_at")
-    ordering = ('-id'),
-    list_filter = ('center'),
-    search_fields = ('new_vehicle_number',)
+# class vehicalDetailsAdmin(admin.ModelAdmin):
+#     # model = Categories
+#     list_per_page = 15 # No of records per page 
+#     list_display = ('id',"customer_name","account_no","center","executive","segment","product_name","new_vehicle_number","engine_number","chasis_number","created_at")
+#     list_display_links = ('id',"customer_name","account_no","center","executive","segment","product_name","new_vehicle_number","engine_number","chasis_number","created_at")
+#     ordering = ('-id'),
+#     list_filter = ('center'),
+#     search_fields = ('new_vehicle_number',)
     
-admin.site.register(Vehicledetails,vehicalDetailsAdmin)
+# admin.site.register(Vehicledetails,vehicalDetailsAdmin)
 
 # admin.site.register(User)
